d442233c0d043e8d9fcc717fc1eb81d7585ad538
Luciana Blümlein <luziehtan@hotmail.com>
2021-03-03 12:09:09 +0100

    Add routes for /api/users and /api/cards

--- server.js
+++ server.js
@@ -2,10 +2,21 @@ const express = require('express')
 
 const app = express()
 
-app.use((req, res) => {
-    console.log(req.method, req.url)
-    res.end('Hello World')
+//Short:
+app.use('/api/users', (req, res) => {
+    res.end('[{"name": "Luciana", "role": "student"}]')
 })
+
+app.use('/api/cards', (req, res) => {
+    res.end('[{"title": "First Card"}]')
+})
+
+/* Long:
+app.use((req, res, next) => {
+    req.url === '/api/cards' 
+    ? res.end('[{"title": "First Card"}]')
+    : next()
+}) */
 
 app.listen(3000, () => {
     console.log('Server started at http://localhost:3000')

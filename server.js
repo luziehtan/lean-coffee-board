059b7a7cb544d6a7a1a486693c75767adee78949
Luciana Blümlein <luziehtan@hotmail.com>
2021-03-03 12:14:04 +0100

    Add prettier and update server.js to receive json data

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const app = express()
 
 //Short:
 app.use('/api/users', (req, res) => {
-    res.end('[{"name": "Luciana", "role": "student"}]')
+  res.json([{ name: 'Luciana', role: 'student' }])
 })
 
 app.use('/api/cards', (req, res) => {
-    res.end('[{"title": "First Card"}]')
+  res.json([{ title: 'First Card' }])
 })
 
 /* Long:
@@ -19,5 +19,5 @@ app.use((req, res, next) => {
 }) */
 
 app.listen(3000, () => {
-    console.log('Server started at http://localhost:3000')
+  console.log('Server started at http://localhost:3000')
 })
